288e6705c73b7517cb17efdcc31f6084da7f2ae6
Cat Ater <103862724@student.swin.edu.au>
2024-08-29 22:49:15 +1000

    Further Commenting and optimisation.
    + Commenting (GameManager).
    + Commenting/Refactor (ObjectMap)
    + Minor Refactor (FloorGeneration).

--- Assets/Scripts/Components/Game Management/GameManager.cs
+++ Assets/Scripts/Components/Game Management/GameManager.cs
@@ -3,13 +3,34 @@ using Assets.Scripts.Types.FloorGeneration;
 
 public class GameManager : MonoBehaviour
 {
+    /// <summary>
+    /// The private singleton reference. 
+    /// </summary>
     private static GameManager _instance;
 
+    /// <summary>
+    /// The current object map.
+    /// </summary>
     public ObjectMap map;
+
+    /// <summary>
+    /// The player prefab used for instancing. . 
+    /// </summary>
     public GameObject playerPrefab;
+
+    /// <summary>
+    /// The current player instance. 
+    /// </summary>
     public GameObject playerCurrent; 
-    public GameObject playerCameraPrefab;
+
+    /// <summary>
+    /// The current floor generation handler instance. 
+    /// </summary>
     public FloorGenerationHandler floorGenerationHandler;
+
+    /// <summary>
+    /// Returns the current instance of the GameManager. 
+    /// </summary>
     public static GameManager Instance => _instance;
 
 
@@ -34,16 +55,28 @@ public class GameManager : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// Set the current camera's position. 
+    /// </summary>
+    /// <param name="position"> The position to set. </param>
     public void SetCamera(Vector3 position)
     {
         Camera.main.transform.position = position;
     }
 
+    /// <summary>
+    /// Spawn a player instance at the specified location. 
+    /// </summary>
+    /// <param name="position"> The position to spawn the player at. </param>
     public void SpawnPlayerInstance(Vector3 position)
     {
         playerCurrent = GameObject.Instantiate(playerPrefab);
         playerCurrent.transform.position = position;
     }
 
+    /// <summary>
+    /// Generates a random map. 
+    /// </summary>
+    /// <param name="data"></param>
     public void GenerateRandomMap(FloorData data) => floorGenerationHandler.GenerateRandomMap(data);
 }
