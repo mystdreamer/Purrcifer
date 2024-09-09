cde84ca445a2861f2844c8af81f50888b981edc4
Cat Ater <103862724@student.swin.edu.au>
2024-09-10 06:01:25 +1000

    Added InZone, OutsideZone

---
+++ Assets/Scripts/Components/Objects/DamageArea/InZone.cs
@@ -0,0 +1,26 @@
+﻿using Purrcifer.Data.Defaults;
+
+public class InZone : ZoneObject
+{
+
+    private void Update()
+    {
+        UpdateSize();
+        if (!insideArea && !ticking)
+        {
+            if (ticking == false && ObjectUpdatable)
+            {
+                ticking = true;
+                GameManager.Instance.playerState.AddDamage = 1;
+                StartCoroutine(CooldownTimer());
+            }
+        }
+        else if (insideArea && ticking)
+        {
+            StopCoroutine(CooldownTimer());
+            ticking = false;
+        }
+    }
+
+    internal override void SetWorldState(WorldStateEnum state) { }
+}
---
+++ Assets/Scripts/Components/Objects/DamageArea/ZoneObject.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public abstract class ZoneObject : RoomObjectBase
+{
+    public ObjectTickDamage damage;
+    public AreaBounds area;
+    public bool ticking = false;
+    public bool insideArea = false;
+    public float cooldownPeriod = 1f;
+    public Vector3 lastSize;
+    public Transform roomParent;
+
+    internal Vector3 GetVector => new Vector3(area.width / 36, 1, area.height / 20);
+
+    public void Start()
+    {
+        UpdateSize();
+    }
+
+    internal override void OnAwakeObject() => ObjectComplete = true;
+
+    internal override void OnSleepObject() { }
+
+    public void OnDrawGizmos()
+    {
+        if (area.transform == null)
+            return;
+        UpdateSize();
+        area.OnDraw();
+    }
+
+    internal void UpdateSize()
+    {
+        if (lastSize != GetVector)
+        {
+            lastSize = GetVector;
+            gameObject.transform.position = area.transform.position;
+            gameObject.transform.localScale = GetVector;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            insideArea = true;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            insideArea = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            insideArea = true;
+    }
+
+    internal IEnumerator CooldownTimer()
+    {
+        yield return new WaitForSeconds(cooldownPeriod);
+        ticking = false;
+    }
+}
