e8eeaf2560b6f911f38541ecd276e715a24e327a
Cat Ater <103862724@student.swin.edu.au>
2024-09-11 05:36:10 +1000

    Added AreaPull.

---
+++ Assets/Scripts/Components/Objects/Zone Objects/Movement Area/Area_Pull.cs
@@ -0,0 +1,41 @@
+﻿using Purrcifer.Data.Defaults;
+using UnityEngine;
+
+public class Area_Pull : InZone
+{
+    public float force;
+    public MovementSys playerMovement;
+
+    internal override void Update()
+    {
+        base.Update();
+
+        Debug.Log("ForcePush: Entered update.");
+
+        if (!InZone)
+            return;
+
+        ///If player reference doesn't exist, cache it. 
+        if (playerMovement == null)
+            playerMovement = GameManager.Instance.playerCurrent.GetComponent<MovementSys>();
+
+        //Apply forces to the object. 
+        if (playerMovement != null)
+        {
+            //Generate the vector between the two points. 
+            Vector3 vectorBetween = transform.position - playerMovement.gameObject.transform.position; 
+
+            Debug.Log("ForcePush: Applying direction.");
+            playerMovement.body.AddForce(vectorBetween.normalized * force, ForceMode.Impulse);
+        }
+    }
+
+    internal override void OnEnterZone() { }
+
+    internal override void OnExitZone() { }
+
+    internal override void SetWorldState(WorldStateEnum state)
+    {
+        //Apply world state changes. 
+    }
+}
