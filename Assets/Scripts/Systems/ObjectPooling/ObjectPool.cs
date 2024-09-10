78ad9836714745fc022f83c6b1079bf7dfb663cc
Cat Ater <103862724@student.swin.edu.au>
2024-09-11 06:49:09 +1000

    Added commenting to the ObjectPoolManage and ObjectPool classes.

---
+++ Assets/Scripts/Systems/ObjectPooling/ObjectPool.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ObjectPool
+{
+    /// <summary>
+    /// The prefab type held by the pool. 
+    /// </summary>
+    public GameObject prefabType;
+
+    /// <summary>
+    /// The pool of objects. 
+    /// </summary>
+    public List<GameObject> pool;
+
+    /// <summary>
+    /// CTOR.
+    /// </summary>
+    /// <param name="prefabType"> The type of GameObject prefab to contain within the pool. </param>
+    public ObjectPool(GameObject prefabType)
+    {
+        this.prefabType = prefabType;
+        this.pool = new List<GameObject>();
+    }
+
+    /// <summary>
+    /// Returns whether the pool is the same type. 
+    /// </summary>
+    /// <param name="prefabType"> The GameObject to locate. </param>
+    /// <returns> True if pool is the same type. </returns>
+    public bool PoolContainsType(GameObject prefabType) =>
+        (prefabType == this.prefabType);
+
+    /// <summary>
+    /// Request an instance of the pools object type. 
+    /// </summary>
+    /// <returns> Instance if a valible, or null if not. </returns>
+    public GameObject RequestInstance()
+    {
+        //Attempt to retrive an object from the object pool. 
+        GameObject retrieved = AttemptPoolFetch();
+
+        //If no object could be retrieved, then attempt to generate a new object. 
+        if (retrieved == null)
+        {
+            retrieved = RequestNewObject();
+            //Add instance to the pool. 
+            pool.Add(retrieved);
+        }
+
+        //Return the instance. 
+        return retrieved;
+    }
+
+    /// <summary>
+    /// Search the pool to see if an object is active. 
+    /// </summary>
+    /// <returns> An instance of the object if the pool or null if not. </returns>
+    private GameObject AttemptPoolFetch()
+    {
+        //Attempt to locate instance. 
+        for (int i = 0; i < pool.Count; i++)
+        {
+            if (!pool[i].activeSelf)
+                return pool[i];
+        }
+
+        //Else return null. 
+        return null;
+    }
+
+    /// <summary>
+    /// Generates a new instance of the GameObject. 
+    /// </summary>
+    /// <returns> New instance of the GameObject. </returns>
+    private GameObject RequestNewObject() =>
+        GameObject.Instantiate(prefabType);
+
+    /// <summary>
+    /// Clear the pool of all objects. 
+    /// </summary>
+    public void EmptyPool()
+    {
+        GameObject obj;
+        for (int i = pool.Count; i > 0; i--)
+        {
+            obj = pool[i];
+            pool[i] = null;
+            GameObject.Destroy(obj);
+        }
+
+        pool = new List<GameObject>();
+    }
+}
--- Assets/Scripts/Systems/ObjectPooling/ObjectPoolManager.cs
+++ Assets/Scripts/Systems/ObjectPooling/ObjectPoolManager.cs
@@ -2,93 +2,70 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class ObjectPool
-{
-    public GameObject prefabType;
-    public List<GameObject> pool;
-    public int limitCap;
-
-    public ObjectPool(GameObject prefabType)
-    {
-        this.prefabType = prefabType;
-        this.pool = new List<GameObject>();
-    }
-
-    public bool PoolContainsType(GameObject prefabType) =>
-        (prefabType == this.prefabType);
-
-    public GameObject RequestInstance()
-    {
-        GameObject retrieved = AttemptPoolFetch();
-
-        if (retrieved == null)
-        {
-            retrieved = RequestNewObject();
-            pool.Add(retrieved);
-        }
-
-        return retrieved;
-    }
-
-    private GameObject AttemptPoolFetch()
-    {
-        for (int i = 0; i < pool.Count; i++)
-        {
-            if (!pool[i].activeSelf)
-                return pool[i];
-        }
-
-        return null;
-    }
-
-    private GameObject RequestNewObject() =>
-        GameObject.Instantiate(prefabType);
-
-    public void EmptyPool()
-    {
-        GameObject obj;
-        for (int i = pool.Count; i > 0; i--)
-        {
-            obj = pool[i];
-            pool[i] = null;
-            GameObject.Destroy(obj);
-        }
-
-        pool = new List<GameObject>();
-    }
-}
-
-[System.Serializable]
 public class ObjectPoolManager
 {
-    public List<ObjectPool> objectPools = new List<ObjectPool>();
+    /// <summary>
+    /// The list of objects held within the manager. 
+    /// </summary>
+    [SerializeField] private List<ObjectPool> objectPools = new List<ObjectPool>();
 
+    /// <summary>
+    /// Generate an object instance.
+    /// </summary>
+    /// <param name="prefab"> The prefab to generate. </param>
+    /// <returns> An instance of the requested GameObject. </returns>
     public GameObject GetGameObjectType(GameObject prefab)
     {
+        //Retrieve the correct pool from the pool list. 
         ObjectPool lPool = LocatePool(prefab);
-        GameObject obj = lPool.RequestInstance();
-        return obj;
+
+        //Request an instance from the pool. 
+        return lPool.RequestInstance();
     }
 
+    /// <summary>
+    /// Generate an object instance.
+    /// </summary>
+    /// <param name="prefab"> The prefab to generate. </param>
+    /// <param name="position"> The position to assign to the object. </param>
+    /// <returns> An instance of the requested GameObject. </returns>
     public GameObject GetGameObjectType(GameObject prefab, Vector2 position)
     {
+        //Get object instance. 
         GameObject obj = GetGameObjectType(prefab);
+
+        //Set position. 
         obj.transform.position = position;
         return obj;
     }
 
-    public GameObject GetGameObjectType(GameObject prefab, GameObject parent, Vector2 position)
+    /// <summary>
+    /// Generate an object instance.
+    /// </summary>
+    /// <param name="prefab"> The prefab to generate. </param>
+    /// <param name="position"> The position to assign to the object. </param>
+    /// <param name="parent"> The parent of the prefab. </param>
+    /// <returns> An instance of the requested GameObject. </returns>
+    public GameObject GetGameObjectType(GameObject prefab, Vector2 position, GameObject parent)
     {
-
+        //Get object instance. 
         GameObject obj = GetGameObjectType(prefab, position);
+        
+        //Set parent. 
         obj.transform.parent = parent.transform;
         return obj;
     }
 
+    /// <summary>
+    /// Returns a pool with the requested GameObject type. 
+    /// </summary>
+    /// <param name="prefab"> The prefab to find the pool of. </param>
     private ObjectPool LocatePool(GameObject prefab)
     {
+        //Cache null pool reference. 
         ObjectPool matchedPool = null;
 
+        //Attempt to match type. 
         for (int i = 0; i < objectPools.Count; i++)
         {
             if (objectPools[i].PoolContainsType(prefab))
@@ -98,25 +75,42 @@ public class ObjectPoolManager
             }
         }
 
+        //If the pool was not located, then generate a new pool instance. 
         if (matchedPool == null)
             matchedPool = AddPool(prefab);
 
+        //Return the pool. 
         return matchedPool;
     }
 
+    /// <summary>
+    /// Add a ObjectPool to the manager. 
+    /// </summary>
+    /// <param name="prefab"> The prefab the pool contains. </param>
+    /// <returns> A new ObjectPool. </returns>
     private ObjectPool AddPool(GameObject prefab)
     {
+        //Generate the pool. 
         ObjectPool p = new ObjectPool(prefab);
+
+        //Add it to the manager. 
         objectPools.Add(p);
         return p;
     }
 
+    /// <summary>
+    /// Clear the pool with the provided type. 
+    /// </summary>
+    /// <param name="prefab"> The type of GameObject to clear from the pool. </param>
     public void ClearPoolByType(GameObject prefab)
     {
         ObjectPool pool = LocatePool(prefab);
         pool.EmptyPool();
     }
 
+    /// <summary>
+    /// Clear all pools from the list. 
+    /// </summary>
     public void ClearPools()
     {
         objectPools = new List<ObjectPool>();
