0b95e47e0ad53c4d7cb4cab0ee7a77300b400736
Samuel Chin <mystdreamer+git@gmail.com>
2024-10-04 08:20:35 +1000

    Fix a compile error

--- Assets/Scripts/Components/Entity/Entity/Enemies/Follow Script/FollowScript.cs
+++ Assets/Scripts/Components/Entity/Entity/Enemies/Follow Script/FollowScript.cs
@@ -1,18 +1,4 @@
 using Purrcifer.Data.Defaults;
-using System.Collections;
-using Unity.VisualScripting;
-using UnityEditor;
-using UnityEngine;
-using UnityEngine.AI;
-
-public class FollowScript : Entity
-{
-    public enum State
-    {
-        INACTIVE,using Purrcifer.Data.Defaults;
-using System.Collections;
-using Unity.VisualScripting;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -80,53 +66,4 @@ public class FollowScript : Entity
     internal override void InvincibilityActivated() { }
 
     internal override void SetWorldState(WorldState state) { }
-}
-
-ACTIVE
-    }
-    public State enemyState = State.INACTIVE;
-public GameObject playerInstance;
-public NavMeshAgent enemy;
-public Vector3 shownPosition;
-
-void Awake()
-{
-    shownPosition = transform.position;
-    enemy = GetComponent<NavMeshAgent>();
-    playerInstance = GameObject.FindWithTag("Player");
-}
-
-void Update()
-{
-    if (enemyState != State.INACTIVE)
-    {
-        transform.LookAt(playerInstance.transform.position);
-        enemy.SetDestination(playerInstance.transform.position);
-    }
-}
-
-internal override void OnAwakeObject()
-{
-    shownPosition = gameObject.transform.position;
-    playerInstance = GameManager.Instance.Player;
-    enemyState = State.ACTIVE;
-}
-
-internal override void OnSleepObject()
-{
-    enemyState = State.INACTIVE;
-}
-internal override void HealthChangedEvent(float lastValue, float currentValue) { }
-
-internal override void OnDeathEvent()
-{
-    gameObject.SetActive(false);
-}
-
-internal override void InvincibilityActivated() { }
-
-internal override void SetWorldState(WorldState state)
-{
-
-}
 }
