8a38c9b148a651bc716c5a02efc894fcb324c8a7
Samuel Chin <mystdreamer+git@gmail.com>
2024-10-04 08:10:45 +1000

    Update FollowScript
    Now activates/deactivates navmeshagent properly on enemy state

--- Assets/Scripts/Components/Entity/Entity/Enemies/Follow Script/FollowScript.cs
+++ Assets/Scripts/Components/Entity/Entity/Enemies/Follow Script/FollowScript.cs
@@ -9,24 +9,38 @@ public class FollowScript : Entity
 {
     public enum State
     {
-        INACTIVE,
-        ACTIVE
-    }
+        INACTIVE,using Purrcifer.Data.Defaults;
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowScript : Entity
+{
     public State enemyState = State.INACTIVE;
     public GameObject playerInstance;
     public NavMeshAgent enemy;
-    public Vector3 shownPosition;    
+    public Vector3 shownPosition;
+
+    public enum State
+    {
+        INACTIVE,
+        ACTIVE
+    }
 
     void Awake()
     {
         shownPosition = transform.position;
         enemy = GetComponent<NavMeshAgent>();
-        playerInstance = GameObject.FindWithTag("Player");
+        playerInstance = GameManager.Instance.Player;
+        // Initially disable the NavMeshAgent
+        enemy.enabled = false;
     }
 
     void Update()
     {
-        if (enemyState != State.INACTIVE) 
+        if (enemyState == State.ACTIVE)
         {
             transform.LookAt(playerInstance.transform.position);
             enemy.SetDestination(playerInstance.transform.position);
@@ -37,24 +51,82 @@ public class FollowScript : Entity
     {
         shownPosition = gameObject.transform.position;
         playerInstance = GameManager.Instance.Player;
+
+        // Set enemy state to active and enable NavMeshAgent
         enemyState = State.ACTIVE;
+        enemy.enabled = true;
+
+        Debug.Log("Enemy is active and NavMeshAgent enabled.");
     }
 
     internal override void OnSleepObject()
     {
-       enemyState = State.INACTIVE;
+        // Set enemy state to inactive and disable NavMeshAgent
+        enemyState = State.INACTIVE;
+        enemy.enabled = false;
+
+        Debug.Log("Enemy is inactive and NavMeshAgent disabled.");
     }
+
     internal override void HealthChangedEvent(float lastValue, float currentValue) { }
 
     internal override void OnDeathEvent()
     {
+        // Optionally disable the NavMeshAgent when the enemy dies
+        enemy.enabled = false;
         gameObject.SetActive(false);
     }
 
     internal override void InvincibilityActivated() { }
 
-    internal override void SetWorldState(WorldState state)
-    {
+    internal override void SetWorldState(WorldState state) { }
+}
 
+ACTIVE
     }
+    public State enemyState = State.INACTIVE;
+public GameObject playerInstance;
+public NavMeshAgent enemy;
+public Vector3 shownPosition;
+
+void Awake()
+{
+    shownPosition = transform.position;
+    enemy = GetComponent<NavMeshAgent>();
+    playerInstance = GameObject.FindWithTag("Player");
+}
+
+void Update()
+{
+    if (enemyState != State.INACTIVE)
+    {
+        transform.LookAt(playerInstance.transform.position);
+        enemy.SetDestination(playerInstance.transform.position);
+    }
+}
+
+internal override void OnAwakeObject()
+{
+    shownPosition = gameObject.transform.position;
+    playerInstance = GameManager.Instance.Player;
+    enemyState = State.ACTIVE;
+}
+
+internal override void OnSleepObject()
+{
+    enemyState = State.INACTIVE;
+}
+internal override void HealthChangedEvent(float lastValue, float currentValue) { }
+
+internal override void OnDeathEvent()
+{
+    gameObject.SetActive(false);
+}
+
+internal override void InvincibilityActivated() { }
+
+internal override void SetWorldState(WorldState state)
+{
+
+}
 }
