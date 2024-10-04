e2847b94f1981984d51205a806c6de2cf6477e4a
Cat Ater <103862724@student.swin.edu.au>
2024-10-04 10:49:26 +1000

    Talisman Collision Check.

--- Assets/Scripts/Components/Objects/Player Objects/TalismanEffector.cs
+++ Assets/Scripts/Components/Objects/Player Objects/TalismanEffector.cs
@@ -9,46 +9,45 @@ public class TalismanEffector : MonoBehaviour
 
     void Start()
     {
+        radiusCollider.radius = maxRaidus;
         StartCoroutine(ExpansionEffect());
-    }
-
-    public void OnCollisionEnter(Collision collision)
-    {
-        ResolveCollision(collision.gameObject);
-    }
-
-    public void OnTriggerEnter(Collider other)
-    {
-        ResolveCollision(other.gameObject);
-    }
-
-    public void ResolveCollision(GameObject obj)
-    {
-        Debug.Log("Talisman collision resolution called");
-        if(obj.GetComponent<TalismanDestructable>() != null)
-        {
-            Debug.Log("Abstract class found");
-            obj.GetComponent<TalismanDestructable>().Resolve();
-        }
     }
 
     public IEnumerator ExpansionEffect()
     {
         float currentTime = 0;
-        float currentRaidus = 0;
+        float currentRaidus = maxRaidus;
 
-        while (currentRaidus < maxRaidus)
+        while (currentRaidus > 0)
         {
             currentTime += Time.deltaTime;
-            currentRaidus = maxRaidus * currentTime;
+            currentRaidus -= maxRaidus * currentTime;
             radiusCollider.radius = currentRaidus;
+            GetCastResults(transform.position, radiusCollider.radius);
             yield return new WaitForEndOfFrame();
         }
 
-        radiusCollider.radius = maxRaidus; 
+        radiusCollider.radius = 0; 
         yield return new WaitForSeconds(0.2F);
 
         gameObject.SetActive(false);
+    }
+
+    private void GetCastResults(Vector3 position, float radius)
+    {
+        RaycastHit[] results = Physics.SphereCastAll(position, radius, transform.forward);
+        GameObject obj;
+
+        foreach (RaycastHit hit in results)
+        {
+            obj = hit.transform.gameObject;
+            Debug.Log("Talisman collision resolution called");
+            if (obj.GetComponent<TalismanDestructable>() != null)
+            {
+                Debug.Log("Abstract class found");
+                obj.GetComponent<TalismanDestructable>().Resolve();
+            }
+        }
     }
 
     public void OnDrawGizmos()
