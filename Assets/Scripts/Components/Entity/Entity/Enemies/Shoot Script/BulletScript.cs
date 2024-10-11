390357d2cb509d6ab44dd6df0c7088fe2faa19dd
Samuel Chin <mystdreamer+git@gmail.com>
2024-10-11 13:06:11 +1100

    Update BulletScript
    uses layer instead of tag

--- Assets/Scripts/Components/Entity/Entity/Enemies/Shoot Script/BulletScript.cs
+++ Assets/Scripts/Components/Entity/Entity/Enemies/Shoot Script/BulletScript.cs
@@ -1,16 +1,18 @@
-using Purrcifer.Data.Defaults;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class BulletScript : MonoBehaviour
 {
+    [SerializeField] private GameObject impactEffect; // The impact effect to instantiate
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Wall")
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
+            // Instantiate the impact effect at the collision point
+            Instantiate(impactEffect, collision.contacts[0].point, Quaternion.identity);
+
+            // Destroy the current GameObject
             Destroy(gameObject);
         }
-        
     }
 }
