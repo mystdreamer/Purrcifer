03b213ad7b949f6f5e575708020f42363d950d21
Cat Ater <103862724@student.swin.edu.au>
2024-10-22 23:35:42 +1100

    Auto stash before merge of "origin/main" and "BossSpawning"

--- Assets/Scripts/Components/Entity/Boss/BossHitbox.cs
+++ Assets/Scripts/Components/Entity/Boss/BossHitbox.cs
@@ -7,28 +7,30 @@ public class BossHitbox : MonoBehaviour
 
     public void OnCollisionEnter(Collision collision)
     {
-        
+        ResolveCollisions(collision.gameObject);
     }
 
     public void OnCollisionStay(Collision collision)
     {
-        
+        ResolveCollisions(collision.gameObject);
     }
 
     public void OnTriggerEnter(Collider other)
     {
-        
+        ResolveCollisions(other.gameObject);
     }
 
     public void OnTriggerStay(Collider other)
     {
-        
+        ResolveCollisions(other.gameObject);
     }
 
     public void ResolveCollisions(GameObject collisionObject)
     {
-        if(collisionObject.tag == "Weapon")
+        Debug.Log("Boss collision hit.");
+        if (collisionObject.tag == "Weapon")
         {
+            Debug.Log("Boss collision hit: Applying damage.");
             boss.CurrentHealth -= GameManager.Instance.PlayerState.Damage;
         }
     }
