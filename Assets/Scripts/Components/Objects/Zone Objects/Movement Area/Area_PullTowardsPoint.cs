1a401fef4c8af37a7e367ed1a87ca9278a2e8c8d
Cat Ater <103862724@student.swin.edu.au>
2024-10-26 04:43:06 +1100

    Area_PullTestRoom added

--- Assets/Scripts/Components/Objects/Zone Objects/Movement Area/Area_PullTowardsPoint.cs
+++ Assets/Scripts/Components/Objects/Zone Objects/Movement Area/Area_PullTowardsPoint.cs
@@ -7,13 +7,25 @@ public class Area_PullTowardsPoint : RoomObjectBase
     public ObjectPhysFxPullTowardsPoint pullTowards;
     bool isActive = false;
 
+    private void Awake()
+    {
+        if(activationType == ObjectActivationType.ON_OBJECT_START)
+            isActive = true;
+    }
+
     internal override void OnAwakeObject()
     {
+        if (activationType == ObjectActivationType.ON_OBJECT_START)
+            return;
+
         isActive = true;
     }
 
     internal override void OnSleepObject()
     {
+        if (activationType == ObjectActivationType.ON_OBJECT_START)
+            return;
+
         isActive = false;
     }
 
